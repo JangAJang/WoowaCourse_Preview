0916d788f38133313de5575ad63e8b7c152a5126
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:27:30 +0900

    feat: RandomNumbers 랜덤한 숫자 3개의 리스트를 생성하는 기능 구현

--- 2주차_숫자_야구_게임_re/src/main/java/baseball/domain/RandomNumbers.java
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/domain/RandomNumbers.java
@@ -1,14 +1,21 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumbers {
 
+    private static final int MINIMUM_IN_RANGE = 1;
+    private static final int MAXIMUM_IN_RANGE = 9;
+
     private final List<Integer> numbers = new ArrayList<>();
 
     public RandomNumbers(){
-
+        while(numbers.size() < 3){
+            numbers.add(Randoms.pickNumberInRange(MINIMUM_IN_RANGE, MAXIMUM_IN_RANGE));
+        }
     }
 
     public int countStrike(List<Integer> playerNumbers){
