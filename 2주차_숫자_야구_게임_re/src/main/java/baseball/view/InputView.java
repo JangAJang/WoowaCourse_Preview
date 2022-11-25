a784dc2d86ab28c572ae5022b6aea357ffdca825
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:46:47 +0900

    feat: InputView 입력문구를 반환하는 기능 구현

--- 2주차_숫자_야구_게임_re/src/main/java/baseball/view/InputView.java
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/view/InputView.java
@@ -1,10 +1,16 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     private static final String INPUT_REQUEST = "숫자를 입력해주세요 : ";
 
-    public void requestNumbers(){
+    private void requestNumbers(){
         System.out.print(INPUT_REQUEST);
+    }
+
+    private String scanInput(){
+        return Console.readLine();
     }
 }
