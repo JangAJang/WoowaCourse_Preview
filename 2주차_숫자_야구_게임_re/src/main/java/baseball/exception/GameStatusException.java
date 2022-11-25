cf8ab88c4e4ee9c3f1f7a514fec6ded4ad90af11
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:43:04 +0900

    feat: GameStatusException 입력이 1이 아닐때 참을 반환하는 기능 구현

--- 2주차_숫자_야구_게임_re/src/main/java/baseball/exception/GameStatusException.java
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/exception/GameStatusException.java
@@ -2,12 +2,15 @@ package baseball.exception;
 
 public class GameStatusException {
 
+    private static final String ONE = "1";
+    private static final String TWO = "2";
+
     public String validate(String input){
         return input;
     }
 
     private boolean isNotOne(String input){
-        return false;
+        return input.equals(ONE);
     }
 
     private boolean isNotTwo(String input){
