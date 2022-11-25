87224993a8e9862fa5218852a8e04be7d9e195ce
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:34:20 +0900

    feat: Player 문자열을 입력해 정수형 리스트로 저장하는 기능 구현

--- 2주차_숫자_야구_게임_re/src/main/java/baseball/domain/Player.java
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/domain/Player.java
@@ -1,14 +1,16 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
 
-    private List<Integer> numbers = new ArrayList<>();
+    private List<Integer> numbers;
 
     public Player(String input){
-
+        numbers = Arrays.stream(input.split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
 
     public List<Integer> getNumbers(){
