fb9d43e4fba39a3f8dcaa26265c8667c528294e8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 14:34:32 +0900

    docs: Numbers도메인 RandomNumbers로 변경

---
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/domain/RandomNumbers.java
@@ -0,0 +1,18 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class RandomNumbers {
+
+    public RandomNumbers(){
+
+    }
+
+    public int countStrike(List<Integer> playerNumbers){
+        return 1;
+    }
+
+    public int countBall(List<Integer> playerNumbers){
+        return 1;
+    }
+}
---
+++ 2주차_숫자_야구_게임_re/src/test/java/baseball/domainTest/RandomNumbersTest.java
@@ -0,0 +1,4 @@
+package baseball.domainTest;
+
+public class RandomNumbersTest {
+}
