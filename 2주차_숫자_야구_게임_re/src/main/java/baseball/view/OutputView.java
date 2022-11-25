cc35f07aec5c423b5f8563db0619e8949080435b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:53:54 +0900

    feat: OutputView 게임시작을 출력하는 기능 구현

---
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,14 @@
+package baseball.view;
+
+public class OutputView {
+
+    private static final String STRIKE_COMMAND = "스트라이크";
+    private static final String BALL_COMMAND = "볼 ";
+    private static final String NOTHING = "낫싱";
+    private static final String START_COMMAND = "숫자 야구 게임을 시작합니다.";
+    private static final String END_COMMAND = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public void startGame(){
+        System.out.println(START_COMMAND);
+    }
+}
