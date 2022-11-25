a416be71bf1b91e7befb2b7a89a202ed55465b10
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:40:58 +0900

    feat: PlayerInputExceptin 예외처리하는 기능 구현

--- 2주차_숫자_야구_게임_re/src/main/java/baseball/exception/PlayerInputException.java
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/exception/PlayerInputException.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public class PlayerInputException {
 
+    private static final String NOT_RIGHT_PLAYER_INPUT_EXCEPTION = "[ERROR] 입력은 1~9로 이루어진 숫자 3개여야 합니다";
+
     public String validate(String input){
         return input;
     }
@@ -21,6 +23,7 @@ public class PlayerInputException {
     }
 
     private void notRightPlayerNumberException(){
-
+        System.out.println(NOT_RIGHT_PLAYER_INPUT_EXCEPTION);
+        throw new IllegalArgumentException();
     }
 }
