a52ce63f3afe8c2585ea4a03190173c0d500bd58
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-25 15:46:10 +0900

    feat: InputView 입력을 요청하는 기능 구현

---
+++ 2주차_숫자_야구_게임_re/src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class InputView {
+
+    private static final String INPUT_REQUEST = "숫자를 입력해주세요 : ";
+
+    public void requestNumbers(){
+        System.out.print(INPUT_REQUEST);
+    }
+}
