41478ca125e14fc6dd0c702be50262cc85a2e4b4
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-27 19:57:42 +0900

    feat: InputView 입력 요청을 출력하고 입력을 반환하는 기능 구현

--- 3주차_로또_re/src/main/java/lotto/view/InputView.java
+++ 3주차_로또_re/src/main/java/lotto/view/InputView.java
@@ -1,9 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    private static final String REQUEST_PAYMENT = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_WINNING = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS = "보너스 번호를 입력해 주세요.";
+
     private String readInput(String comment){
-        return null;
+        System.out.println(comment);
+        return Console.readLine();
     }
 
     public String readPayment(){
