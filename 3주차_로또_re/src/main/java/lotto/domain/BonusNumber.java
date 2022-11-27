abcbc0a8f42c21e0773d431bdc7751f1cce6d383
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-27 19:38:36 +0900

    feat: BonusNumber 생성자에서 예외를 확인하고 초기화시키는 기능 구현

--- 3주차_로또_re/src/main/java/lotto/domain/BonusNumber.java
+++ 3주차_로또_re/src/main/java/lotto/domain/BonusNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.exception.BonusNumberException;
+
 import java.util.List;
 
 public class BonusNumber {
@@ -7,7 +9,8 @@ public class BonusNumber {
     private int bonusNumber;
 
     public BonusNumber(Lotto winningLotto, String input){
-
+        BonusNumberException bonusNumberException = new BonusNumberException();
+        bonusNumber = bonusNumberException.validate(input, winningLotto);
     }
 
     public boolean isHavingBonusNumber(List<Integer> numbers){
