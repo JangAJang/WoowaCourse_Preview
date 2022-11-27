f9939985d5de3056368a5f87be08df14d752bb88
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-27 19:09:31 +0900

    feat: PriceInputException 입력이 숫자가 아니면 참을 반환하는 기능 구현

--- 3주차_로또_re/src/main/java/lotto/exception/PriceInputException.java
+++ 3주차_로또_re/src/main/java/lotto/exception/PriceInputException.java
@@ -1,4 +1,14 @@
 package lotto.exception;
 
+import java.util.regex.Pattern;
+
 public class PriceInputException {
+
+    public Long validate(String input){
+        return 0L;
+    }
+
+    private boolean isNotNumber(String input){
+        return !Pattern.matches("^[0-9]*$", input);
+    }
 }
--- 3주차_로또_re/src/test/java/lotto/exceptionTest/PriceInputExceptionTest.java
+++ 3주차_로또_re/src/test/java/lotto/exceptionTest/PriceInputExceptionTest.java
@@ -1,4 +1,36 @@
 package lotto.exceptionTest;
 
+import lotto.exception.PriceInputException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 public class PriceInputExceptionTest {
+
+    PriceInputException priceInputException = new PriceInputException();
+
+    @DisplayName("입력이 숫자가 아니면 예외처리")
+    @Test
+    void isNOtNumberTest(){
+        assertThatThrownBy(()->priceInputException.validate("10000j"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("1,000으로 나눈 몫이 0일 때 예외처리한다. ")
+    @Test
+    void isNotDividedByThousand1(){
+        assertThatThrownBy(()->priceInputException.validate("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("1,000으로 나눈 나머지가 0이 아닐 때 예외처리한다. ")
+    @Test
+    void isNotDividedByThousand2(){
+        assertThatThrownBy(()->priceInputException.validate("1900"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("예외처리가 될게 없다면 Long 타입으로 반환된다. ")
+    @Test
+    void validateTest(){
+        assertThat(priceInputException.validate("2000")).isEqualTo(2000L);
+    }
 }
