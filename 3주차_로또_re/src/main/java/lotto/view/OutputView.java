775e3104a943788ad056ff4604c3a12a4e1c74e3
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-27 20:01:29 +0900

    feat: OutputView 플레이어의 구매 결과를 출력하는 기능 구현

--- 3주차_로또_re/src/main/java/lotto/view/OutputView.java
+++ 3주차_로또_re/src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,21 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
+    private static final String STARTER = "당첨통계\n---";
+    private static final String BOUGHT_COUNT = "개를 구매했습니다.";
+
     public void printStart(){
-        System.out.println("당첨통계\n---");
+        System.out.println(STARTER);
     }
 
-    public void printPlayerNumber(){
-
+    public void printPlayerNumber(List<List<Integer>> playerNumbers){
+        System.out.println(playerNumbers.size() + BOUGHT_COUNT);
+        for(List<Integer> numbersEach : playerNumbers){
+            System.out.println(numbersEach);
+        }
     }
 
     public void printResult(){
