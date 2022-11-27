ef97bb386879807de248141c2bb79c73dbba80ba
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-27 19:12:19 +0900

    feat: PriceInputException 예외를 확인하고 예외가 없을 시 Long타입으로 반환하는 기능 구현

--- 3주차_로또_re/src/main/java/lotto/exception/PriceInputException.java
+++ 3주차_로또_re/src/main/java/lotto/exception/PriceInputException.java
@@ -4,8 +4,11 @@ import java.util.regex.Pattern;
 
 public class PriceInputException {
 
+    private static final String NOT_RIGHT_PAYMENT_COMMENT = "[ERROR] 금액은 1000원단위로 등록해야 합니다. ";
+
     public Long validate(String input){
-        return 0L;
+        if(isNotNumber(input) || isNotDividedByThousand(input)) notRightPaymentException();
+        return Long.parseLong(input);
     }
 
     private boolean isNotNumber(String input){
@@ -14,5 +17,10 @@ public class PriceInputException {
 
     private boolean isNotDividedByThousand(String input){
         return Long.parseLong(input) % 1000 != 0 || Long.parseLong(input) / 1000 ==0;
+    }
+
+    private void notRightPaymentException(){
+        System.out.println(NOT_RIGHT_PAYMENT_COMMENT);
+        throw new IllegalArgumentException();
     }
 }
