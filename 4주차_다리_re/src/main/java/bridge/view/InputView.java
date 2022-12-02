242a08f59f8ee85b213351d3fd05dbbd6e2b8db7
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:19:46 +0900

    feat: InputView 다리 길이를 요청할 때, 예외가 발생하면 다시 실행해 올바른 길이를 반환하는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/view/InputView.java
+++ 4주차_다리_re/src/main/java/bridge/view/InputView.java
@@ -1,5 +1,6 @@
 package bridge.view;
 
+import bridge.validator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -11,13 +12,19 @@ public class InputView {
     private static final String REQUEST_BRIDGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String REQUEST_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
+    private final InputValidator inputValidator = new InputValidator();
+
     private String readInput(String request){
         System.out.println(request);
         return Console.readLine();
     }
 
     public int readBridgeSize() {
-        return 0;
+        try{
+            return inputValidator.validateLength(readInput(REQUEST_LENGTH));
+        }catch (IllegalArgumentException e){
+            return readBridgeSize();
+        }
     }
 
     /**
