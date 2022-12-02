72072dd8be3fc6b914ca5808087fa22e0ba91572
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:26:00 +0900

    feat: OutputView 최종 결과를 출력하는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/view/OutputView.java
+++ 4주차_다리_re/src/main/java/bridge/view/OutputView.java
@@ -1,8 +1,13 @@
 package bridge.view;
 
+import static bridge.enums.GameStatus.GOING;
+import static bridge.enums.GameStatus.QUIT;
+
 public class OutputView {
 
     private static final String FINAL_RESULT = "최종 게임 결과";
+    private static final String GAME_STATUS = "게임 성공 여부: ";
+    private static final String TOTAL_TRIAL = "총 시도한 횟수: ";
 
     public void printMap(String map) {
         System.out.println(map);
@@ -13,6 +18,13 @@ public class OutputView {
         printMap(map);
     }
 
-    public void printResult() {
+    public void printResult(boolean isGameQuit, int trialCount){
+        System.out.println(GAME_STATUS + printGameStatus(isGameQuit));
+        System.out.println(TOTAL_TRIAL + trialCount);
+    }
+
+    private String printGameStatus(boolean isGameQuit){
+        if(isGameQuit) return QUIT.getStatement();
+        return GOING.getStatement();
     }
 }
