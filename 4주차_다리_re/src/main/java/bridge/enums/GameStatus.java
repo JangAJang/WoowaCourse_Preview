621fecc3fa156a28c43f80dce66d6ac8cfb5e6c8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 15:34:31 +0900

    feat: GameStatus 속성들과 호출메서드 구현

--- 4주차_다리_re/src/main/java/bridge/enums/GameStatus.java
+++ 4주차_다리_re/src/main/java/bridge/enums/GameStatus.java
@@ -1,4 +1,22 @@
 package bridge.enums;
 
 public enum GameStatus {
+
+    GOING("R", "성공"), QUIT("Q", "실패");
+
+    private String command;
+    private String statement;
+
+    private GameStatus(String command, String statement){
+        this.command = command;
+        this.statement = statement;
+    }
+
+    public String getCommand(){
+        return this.command;
+    }
+
+    public String getStatement(){
+        return this.statement;
+    }
 }
