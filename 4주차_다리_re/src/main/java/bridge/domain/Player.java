77ac30a113d0fdb03ee7430cbcea417dfd91414b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 15:40:35 +0900

    feat: Player 입력값을 인스턴스에 저장하는 기능 구현'

--- 4주차_다리_re/src/main/java/bridge/domain/Player.java
+++ 4주차_다리_re/src/main/java/bridge/domain/Player.java
@@ -1,5 +1,6 @@
 package bridge.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
@@ -7,11 +8,11 @@ public class Player {
     List<Boolean> playerAnswers;
 
     public Player(){
-
+        playerAnswers = new ArrayList<>();
     }
 
     public void addPlayerAnswer(boolean result){
-
+        playerAnswers.add(result);
     }
 
     public boolean isFailedGame(){
