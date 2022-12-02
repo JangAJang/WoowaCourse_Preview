54c1566c2924277e6e64ee50b9e0de3e2c6d7cb7
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:10:14 +0900

    feat: InputValidator 길이 입력이 숫자가 아니거나 범위 밖이면 예외처리하고 이상 없을 시 정수형으로 반환하는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/validator/InputValidator.java
+++ 4주차_다리_re/src/main/java/bridge/validator/InputValidator.java
@@ -1,9 +1,32 @@
 package bridge.validator;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
 
+    private static final int MINIMUM = 3;
+    private static final int MAXIMUM = 20;
+    private static final String LENGTH_EXCEPTION  ="[ERROR] 다리의 길이는 3부터 20의 정수여야 합니다.";
+
+
     public int validateLength(String input){
-        return 0;
+        if(isNotNumber(input) || isOutOfRange(Integer.parseInt(input)))
+            throwLengthException();
+        return Integer.parseInt(input);
+    }
+
+    private boolean isNotNumber(String input){
+        return !Pattern.matches("^[0-9]*$", input);
+    }
+
+    private boolean isOutOfRange(int input){
+        return input < MINIMUM || input > MAXIMUM;
+    }
+
+    private void throwLengthException(){
+        System.out.println(LENGTH_EXCEPTION);
+        throw new IllegalArgumentException();
+
     }
 
     public String validateBridgeChoice(String input){
