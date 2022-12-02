31081b25512c11f04bc395a9a2a5288743d6ff40
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:20:31 +0900

    feat: InputView 이동할 다리를 요청할 때, 예외가 발생하면 다시 실행해 올바른 다리를 반환하는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/view/InputView.java
+++ 4주차_다리_re/src/main/java/bridge/view/InputView.java
@@ -27,11 +27,12 @@ public class InputView {
         }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        try{
+            return inputValidator.validateBridgeChoice(readInput(REQUEST_BRIDGE));
+        }catch (IllegalArgumentException e){
+            return readMoving();
+        }
     }
 
     /**
