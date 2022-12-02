9422ba1cb68fdcf056f93e4197dd365cbb27470b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:18:20 +0900

    feat: InputView 입력을 요청하고 반환하는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/view/InputView.java
+++ 4주차_다리_re/src/main/java/bridge/view/InputView.java
@@ -1,13 +1,21 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+    private static final String REQUEST_LENGTH = "다리의 길이를 입력해주세요.";
+    private static final String REQUEST_BRIDGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String REQUEST_RETRY = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
+    private String readInput(String request){
+        System.out.println(request);
+        return Console.readLine();
+    }
+
     public int readBridgeSize() {
         return 0;
     }
