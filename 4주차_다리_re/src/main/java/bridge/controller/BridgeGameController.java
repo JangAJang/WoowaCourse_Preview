98a33cafb2ba0ec38b5bac77bb82cbdad6960285
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:27:26 +0900

    feat: BridgeGameController 생성자로 BridgeGame을 입력받은 사이즈로 생성해주는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/controller/BridgeGameController.java
+++ 4주차_다리_re/src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,16 @@
 package bridge.controller;
 
+import bridge.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeGameController {
+
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final BridgeGame bridgeGame;
+
+    public BridgeGameController(){
+        bridgeGame = new BridgeGame(inputView.readBridgeSize());
+    }
 }
