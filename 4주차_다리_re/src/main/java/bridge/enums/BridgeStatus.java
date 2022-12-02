dc152b21b11e2e67f8392d6603e059d3cee65615
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 15:33:08 +0900

    feat: BirdgeStatus enum타입 값과 호출메서드 구현

--- 4주차_다리_re/src/main/java/bridge/enums/BridgeStatus.java
+++ 4주차_다리_re/src/main/java/bridge/enums/BridgeStatus.java
@@ -1,4 +1,23 @@
 package bridge.enums;
 
 public enum BridgeStatus {
+
+    UPPER(1, "U"), DOWN(0, "D");
+
+
+    private int value;
+    private String command;
+
+    private BridgeStatus(int value, String command){
+        this.value = value;
+        this.command = command;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public String getCommand(){
+        return this.command;
+    }
 }
