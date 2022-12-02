5cca756eda7585d4030977578f55dde7962a6308
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 15:35:47 +0900

    feat: BridgeMakingElement 속성들과 호출 메서드 구현

--- 4주차_다리_re/src/main/java/bridge/enums/BridgeMakingElements.java
+++ 4주차_다리_re/src/main/java/bridge/enums/BridgeMakingElements.java
@@ -1,4 +1,21 @@
 package bridge.enums;
 
 public enum BridgeMakingElements {
+
+    STARTER("[ "),
+    FINISHER("]"),
+    CORRECT("O "),
+    WRONG("X "),
+    EMPTY(" "),
+    DIVIDER("| ");
+
+    private String element;
+
+    private BridgeMakingElements(String element){
+        this.element = element;
+    }
+
+    public String getElement(){
+        return this.element;
+    }
 }
