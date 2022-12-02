359dfc0a4d35835409533d79ff63894ab6c8ca9c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-02 16:21:53 +0900

    feat: InputView 게임 종료여부를 요청할 때, 예외가 발생시 다시 요청하고 올바른 문자열을 반환하는 기능 구현

--- 4주차_다리_re/src/main/java/bridge/view/InputView.java
+++ 4주차_다리_re/src/main/java/bridge/view/InputView.java
@@ -34,11 +34,11 @@ public class InputView {
             return readMoving();
         }
     }
-
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        try{
+            return inputValidator.validateGameRetrialChoice(readInput(REQUEST_RETRY));
+        }catch (IllegalArgumentException e){
+            return readGameCommand();
+        }
     }
 }
