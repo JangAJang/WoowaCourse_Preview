0292e5b43a38eebf84a46aead737b426a6000dc1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:05:06 +0900

    feat: InputValidator 이름의 길이가 5를 넘으면 참을 반환하는 기능 구현

--- 자동차 경주 게임/src/main/java/racingcar/validator/InputValidator.java
+++ 자동차 경주 게임/src/main/java/racingcar/validator/InputValidator.java
@@ -6,11 +6,17 @@ import java.util.stream.Collectors;
 
 public class InputValidator {
 
+    private static final int LENGTH_LIMIT = 5;
+
     public List<String> validateNames(String input){
         List<String> names = separateNames(input);
     }
 
     private List<String> separateNames(String input){
         return Arrays.stream(input.split(",")).collect(Collectors.toList());
+    }
+
+    private boolean isEachNameTooLong(String name){
+        return name.length() > LENGTH_LIMIT;
     }
 }
