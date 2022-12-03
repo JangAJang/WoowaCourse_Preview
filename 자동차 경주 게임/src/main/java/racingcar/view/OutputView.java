dbdd802e27c2b493284e8598355d8aa15c7ff906
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:17:02 +0900

    feat: OutputView 실행결과 출력의 시작을 알려주는 기능 구현'

---
+++ 자동차 경주 게임/src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String RESULT = "실행 결과";
+
+    public void printStartOfResult(){
+        System.out.println(RESULT);
+    }
+}
