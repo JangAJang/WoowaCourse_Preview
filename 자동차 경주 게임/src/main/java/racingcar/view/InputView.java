36882920993175f178c15c9c1f4e8b7ef29b08da
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:14:51 +0900

    feat: InputView 시도 횟수를 요청하고 콘솔에서 읽어오는 기능 구현

--- 자동차 경주 게임/src/main/java/racingcar/view/InputView.java
+++ 자동차 경주 게임/src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class InputView {
 
     private static final String REQUEST_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_TRIALS = "시도할 회수는 몇회인가요?";
 
     private InputValidator inputValidator = new InputValidator();
 
@@ -17,10 +18,18 @@ public class InputView {
     }
 
     public List<String> readNames(){
-        return inputValidator.validateNames(scanNames());
+        return validateNameInput(scanNames());
     }
 
     private String scanNames(){
         return readInput(REQUEST_NAMES);
+    }
+
+    private List<String> validateNameInput(String input){
+        return inputValidator.validateNames(input);
+    }
+
+    private String scanTrials(){
+        return readInput(REQUEST_TRIALS);
     }
 }
