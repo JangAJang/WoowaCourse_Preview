dbb9db2211281fffe68d09ef4465374d0bb068d8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:10:44 +0900

    feat: InputView 입력변수를 출력하고 콘솔 입력을 반환하는 기능 구현

---
+++ 자동차 경주 게임/src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private String readInput(String request){
+        System.out.println(request);
+        return Console.readLine();
+    }
+}
