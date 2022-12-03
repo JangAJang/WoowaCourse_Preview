4f8332109210f41d46979b8f7d476cf7d7e5b198
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:21:27 +0900

    feat: OutputView 최종 우승자의 명단을 출력하는 기능 구현

--- 자동차 경주 게임/src/main/java/racingcar/view/OutputView.java
+++ 자동차 경주 게임/src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OutputView {
 
     private static final String RESULT = "실행 결과";
+    private static final String FINAL_WINNERS = "최종 우승자 : ";
 
     public void printStartOfResult(){
         System.out.println(RESULT);
@@ -15,5 +16,11 @@ public class OutputView {
         results.forEach(resultEach->builder
                 .append(resultEach).append("\n"));
         System.out.println(builder);
+    }
+
+    public void printFinalWinners(List<String> names){
+        StringBuilder namePrinter = new StringBuilder();
+        names.forEach(nameEach->namePrinter.append(nameEach).append(","));
+        System.out.println(FINAL_WINNERS + namePrinter.substring(0, namePrinter.length()-1));
     }
 }
