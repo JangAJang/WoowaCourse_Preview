7b2b6081fa4f3cab5f1554a143788846855d351f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:12:36 +0900

    feat: InputView 이름 입력을 요청해 받고 예외처리후 리스트로 반환하는 기능 구현

--- 자동차 경주 게임/src/main/java/racingcar/view/InputView.java
+++ 자동차 경주 게임/src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,22 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.InputValidator;
+
+import java.util.List;
 
 public class InputView {
+
+    private static final String REQUEST_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private InputValidator inputValidator = new InputValidator();
 
     private String readInput(String request){
         System.out.println(request);
         return Console.readLine();
+    }
+
+    public List<String> readNames(){
+        return inputValidator.validateNames(readInput(REQUEST_NAMES));
     }
 }
