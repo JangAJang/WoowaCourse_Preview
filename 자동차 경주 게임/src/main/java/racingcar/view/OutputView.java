36c1d03656b52d0d7289d4bc809944228c582123
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-03 13:18:50 +0900

    feat: OutputView 자동차의 결과들을 출력해주는 기능 구현

--- 자동차 경주 게임/src/main/java/racingcar/view/OutputView.java
+++ 자동차 경주 게임/src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,19 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String RESULT = "실행 결과";
 
     public void printStartOfResult(){
         System.out.println(RESULT);
+    }
+
+    public void printCarsPositions(List<String> results){
+        StringBuilder builder = new StringBuilder();
+        results.forEach(resultEach->builder
+                .append(resultEach).append("\n"));
+        System.out.println(builder);
     }
 }
