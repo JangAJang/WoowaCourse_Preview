5f45840165aadcccb9744865ac70e07a950c3115
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-04 16:36:47 +0900

    feat: InputValidator 금액의 입력에 대한 예외를 확인하고 없을 떄 정수형으로 반환하는 기능 구현

--- 자판기/src/main/java/vendingmachine/validator/InputValidator.java
+++ 자판기/src/main/java/vendingmachine/validator/InputValidator.java
@@ -4,8 +4,12 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
 
-    public int validatePrice(String input){
+    private static final String NOT_RIGHT_PRICE = "[ERROR]금액은 100원부터 시작하며, 10원으로 나누어떨어져야 한다.";
 
+    public int validatePrice(String input){
+        if(isNotNumber(input) || isNotDividedByTen(input) || isLessThanHundred(input))
+            notRightPriceException();
+        return Integer.parseInt(input);
     }
 
     private boolean isNotNumber(String input){
@@ -18,5 +22,10 @@ public class InputValidator {
 
     private boolean isLessThanHundred(String input){
         return Integer.parseInt(input) < 100;
+    }
+
+    private void notRightPriceException(){
+        System.out.println(NOT_RIGHT_PRICE);
+        throw new IllegalArgumentException();
     }
 }
