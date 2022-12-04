96760efdde960d903d97a9e181bdbbb94a7f4777
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-04 17:09:12 +0900

    feat: VendingMachineController 잔돈을 입력시켜 자판기를 생성하는 기능 구현

--- 자판기/src/main/java/vendingmachine/controller/VendingMachineController.java
+++ 자판기/src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,4 +1,16 @@
 package vendingmachine.controller;
 
+import vendingmachine.domainModel.VendingMachine;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
 public class VendingMachineController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final VendingMachine vendingMachine;
+
+    public VendingMachineController(){
+        vendingMachine = new VendingMachine(inputView.readPrice());
+    }
 }
