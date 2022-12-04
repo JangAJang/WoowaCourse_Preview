1d4d4271344c3f49aba167476b6e3ada87fa0c68
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-04 16:33:53 +0900

    feat: InputValidator 숫자가 아닐 때 참을 반환하는 기능 구현

--- 자판기/src/main/java/vendingmachine/validator/InputValidator.java
+++ 자판기/src/main/java/vendingmachine/validator/InputValidator.java
@@ -1,4 +1,14 @@
 package vendingmachine.validator;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
+
+    public int validatePrice(String input){
+
+    }
+
+    private boolean isNotNumber(String input){
+        return !Pattern.matches("^[0-9]*$", input);
+    }
 }
