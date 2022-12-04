b78831b1c3a7fa36770ad392059b005af83914e5
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-04 15:41:58 +0900

    feat: Coins 생성자로 코인을 해시맵으로 추가하는 기능 구현

--- 자판기/src/main/java/vendingmachine/domain/Coins.java
+++ 자판기/src/main/java/vendingmachine/domain/Coins.java
@@ -1,15 +1,24 @@
 package vendingmachine.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import vendingmachine.Coin;
 
 import java.util.HashMap;
+
+import static vendingmachine.Coin.*;
 
 public class Coins {
 
     HashMap<Coin, Integer> coinCounter = new HashMap<>();
 
     public Coins(int price){
+        addCoin(COIN_10, addCoin(COIN_50, addCoin(COIN_100,addCoin(COIN_500, price))));
+    }
 
+    private int addCoin(Coin coin, int price){
+        coinCounter.put(coin,
+                Randoms.pickNumberInRange(0, price/coin.getAmount()));
+        return price - coinCounter.get(coin)*coin.getAmount();
     }
 
     public HashMap<Coin, Integer> showAvailableCoins(){
