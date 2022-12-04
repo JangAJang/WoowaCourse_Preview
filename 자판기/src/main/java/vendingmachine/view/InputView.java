80b4ddd432d29de29d8ee3179c9b1967a39d9769
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-04 16:52:03 +0900

    feat: InputView 입력을 요청하고 받아오는 기능 구현'

--- 자판기/src/main/java/vendingmachine/view/InputView.java
+++ 자판기/src/main/java/vendingmachine/view/InputView.java
@@ -1,4 +1,13 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private String readInput(String request){
+        System.out.println(request);
+        return Console.readLine();
+    }
+
+
 }
