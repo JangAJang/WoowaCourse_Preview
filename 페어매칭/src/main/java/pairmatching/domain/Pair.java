27b655a178ea877f40298356f6fb1524b4710c71
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-10 19:14:19 +0900

    feat: Pair 문자열 리스트가 오면 이를 정렬해 인스턴스 초기화시키는 기능 구현

--- 페어매칭/src/main/java/pairmatching/domain/Pair.java
+++ 페어매칭/src/main/java/pairmatching/domain/Pair.java
@@ -1,13 +1,14 @@
 package pairmatching.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Pair {
 
     private final List<String> names;
 
     public Pair(List<String> names){
-        this.names = names;
+        this.names = names.stream().sorted().collect(Collectors.toList());
     }
 
     public List<String> getNames() {
