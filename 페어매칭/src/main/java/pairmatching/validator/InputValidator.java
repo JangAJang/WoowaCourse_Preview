6cfaba154d6f60e0e04503ed22ad0e086ef7e35b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-10 21:38:47 +0900

    feat: InputValidator 입력이 1부터 3이면 참을 반환하는 기능 구현

--- 페어매칭/src/main/java/pairmatching/validator/InputValidator.java
+++ 페어매칭/src/main/java/pairmatching/validator/InputValidator.java
@@ -1,6 +1,6 @@
 package pairmatching.validator;
 
-import static pairmatching.enums.OperationCommand.QUIT;
+import static pairmatching.enums.OperationCommand.*;
 
 public class InputValidator {
 
@@ -10,5 +10,17 @@ public class InputValidator {
 
     private boolean isQ(String input){
         return input.equals(QUIT.getCommand());
+    }
+
+    private boolean isPairMatch(String input){
+        return input.equals(MATCH_PAIR.getCommand());
+    }
+
+    private boolean isReaPair(String input){
+        return input.equals(READ_PAIR.getCommand());
+    }
+
+    private boolean isClear(String input){
+        return input.equals(CLEAR.getCommand());
     }
 }
