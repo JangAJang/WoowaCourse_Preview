f7b089c36a3074e46c3f6b910294935207478b58
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-10 19:28:58 +0900

    feat: Pairs 이름을 개수별로 나누어 Pair의 리스트로 만드는 기능 구현

--- 페어매칭/src/main/java/pairmatching/domain/Pairs.java
+++ 페어매칭/src/main/java/pairmatching/domain/Pairs.java
@@ -8,7 +8,10 @@ public class Pairs {
     List<Pair> pairs = new ArrayList<>();
 
     public Pairs(List<String> names){
-
+        for(int index = 0; index < names.size()/2-1; index+=2){
+            pairs.add(new Pair(names.subList(index, index+2)));
+        }
+        pairs.add(new Pair(names.subList((names.size()/2-1)*2, names.size())));
     }
 
     public List<Pair> getPairs() {
