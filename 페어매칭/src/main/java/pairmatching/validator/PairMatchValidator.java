f0e0e8419ff89646cc1f4725b953b03e605f036e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-10 19:34:36 +0900

    feat: PairMatchingValidator 에서 입력 문자열이 프론트엔드일 때 참을 반환하는 기능 구현

--- 페어매칭/src/main/java/pairmatching/validator/PairMatchValidator.java
+++ 페어매칭/src/main/java/pairmatching/validator/PairMatchValidator.java
@@ -3,6 +3,7 @@ package pairmatching.validator;
 import pairmatching.enums.Course;
 
 import static pairmatching.enums.Course.BACKEND;
+import static pairmatching.enums.Course.FRONTEND;
 
 public class PairMatchValidator {
 
@@ -12,5 +13,9 @@ public class PairMatchValidator {
 
     private boolean isBackEnd(String name){
         return name.equals(BACKEND.getName());
+    }
+
+    private boolean isFrontEnd(String name){
+        return name.equals(FRONTEND.getName());
     }
 }
