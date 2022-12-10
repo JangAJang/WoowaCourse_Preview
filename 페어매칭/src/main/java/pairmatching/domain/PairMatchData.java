17464999e31ee5bacd7a8be71511f546a7c51d1e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-10 21:16:36 +0900

    feat: PairMatchData 에서 3회 연속 같은 페어가 발견되어 종료시키는 기능 구현

--- 페어매칭/src/main/java/pairmatching/domain/PairMatchData.java
+++ 페어매칭/src/main/java/pairmatching/domain/PairMatchData.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class PairMatchData {
 
+    private static final String THREE_DUP_SAME_PAIR = "[ERROR] 페어 매칭을 3회 시도해도 같은 페어가 존재해 정지합니다.";
+
     private final List<PairMatch> pairMatches = new ArrayList<>();
 
     public List<List<String>> createPairMatch(List<String> components){
@@ -21,6 +23,11 @@ public class PairMatchData {
             if(pairMatchEach.hasSamePairBefore(pairMatch)) return true;
         }
         return false;
+    }
+
+    private void threeDuplicatedException(){
+        System.out.println(THREE_DUP_SAME_PAIR);
+        throw new IllegalArgumentException();
     }
 
     public boolean containsExistingDataAlready(List<String> components){
