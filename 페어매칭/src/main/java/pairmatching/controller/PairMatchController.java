2de043ef3ce98e2f28ee44cdb85077158c6444a2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-12-10 22:07:04 +0900

    feat: PairMatchController에서 페어매칭을 진행시키는 기능 구현

---
+++ 페어매칭/src/main/java/pairmatching/controller/PairMatchController.java
@@ -0,0 +1,58 @@
+package pairmatching.controller;
+
+import pairmatching.domain.PairMatchData;
+import pairmatching.enums.OperationCommand;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+import java.util.List;
+
+import static pairmatching.enums.OperationCommand.MATCH_PAIR;
+
+public class PairMatchController {
+
+    private final PairMatchData pairMatchData = new PairMatchData();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    private boolean isPairMatching(OperationCommand command){
+        return command.equals(MATCH_PAIR);
+    }
+
+    private void matchPair(){
+        List<String> component = readPairMatchingMission();
+        if(isAlreadyExistingMission(component)){
+            rematchPair(component);
+            return;
+        }
+        printResult(createPairs(component));
+    }
+
+    private void rematchPair(List<String> component){
+        if(isRequiringRematch()) printResult(recreatePairs(component));
+    }
+
+    private List<String> readPairMatchingMission(){
+        return inputView.readMissionChoice();
+    }
+
+    private boolean isAlreadyExistingMission(List<String> component){
+        return pairMatchData.containsExistingDataAlready(component);
+    }
+
+    private boolean isRequiringRematch(){
+        return inputView.readRematch();
+    }
+
+    private List<List<String>> createPairs(List<String> component){
+        return pairMatchData.createPairMatch(component);
+    }
+
+    private void printResult(List<List<String>> members){
+        outputView.printMatchingResult(members);
+    }
+
+    private List<List<String>> recreatePairs(List<String> component){
+        return pairMatchData.recreatePairMatch(component);
+    }
+}
